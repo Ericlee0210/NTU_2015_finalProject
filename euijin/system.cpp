0e26214062c3427019708a69ac025d0c6599896e
mogandaz <mogandaz@gmail.com>
2015-06-07 12:38:20 +0800

    Make merge function
    Assumed that if merging ID1 and ID2 have transaction between two, just
    leaved them.
    If you want to delete it, then consider other users' log_idx.

--- euijin/system.cpp
+++ euijin/system.cpp
@@ -17,7 +17,7 @@ int main()
     std::istringstream input(row);
     input >> op;
 
-    if (op == "create")
+    if (op == "create") // find 하는데 드는 시간은 log n
     {
       input >> ID1 >> PW1;
       if (T.find(ID1) == T.end()) // 아무것도 없을 때도 성립?
@@ -31,12 +31,13 @@ int main()
         std::cout << "ID " << ID1 << " exists, ";
     }
 
-    else if (op == "login")
+    else if (op == "login") // log n
     {
       input >> ID1 >> PW1;
-      if (T.find(ID1) == T.end())
+      AccountT::iterator id1_pos = T.find(ID1);
+      if (id1_pos == T.end())
         std::cout << "ID " << ID1 << " not found\n";
-      else if (T.find(ID1)->second.password != PW1)
+      else if (id1_pos->second.password != PW1)
         std::cout << "wrong password\n";
       else
       {
@@ -45,12 +46,13 @@ int main()
       }
     }
 
-    else if (op == "delete")
+    else if (op == "delete") // log n
     {
       input >> ID1 >> PW1;
-      if (T.find(ID1) == T.end())
+      AccountT::iterator id1_pos = T.find(ID1);
+      if (id1_pos == T.end())
         std::cout << "ID " << ID1 << " not found\n";
-      else if (T.find(ID1)->second.password != PW1)
+      else if (id1_pos->second.password != PW1)
         std::cout << "wrong password\n";
       else
       {
@@ -59,6 +61,65 @@ int main()
       }
     }
 
+    else if (op == "merge") // n1 n2 -> log n1 log n2 h1 h2
+    {
+      input >> ID1 >> PW1 >> ID2 >> PW2;
+      AccountT::iterator id1_pos = T.find(ID1);
+      AccountT::iterator id2_pos = T.find(ID2);
+      if (id1_pos == T.end())
+        std::cout << "ID " << ID1 << " not found\n";
+      else if (id2_pos == T.end())
+        std::cout << "ID " << ID2 << " not found\n";
+      else if (id1_pos->second.password != PW1)
+        std::cout << "wrong password1\n";
+      else if (id2_pos->second.password != PW2)
+        std::cout << "wrong password2\n";
+      else
+      {
+        std::vector<int>::iterator it1 = id1_pos->second.log_idx.begin();
+        std::vector<int>::iterator it2 = id2_pos->second.log_idx.begin();
+        std::vector<int> temp;
+
+        while (it1 != id1_pos->second.log_idx.end() && it2 != id2_pos->second.log_idx.end())
+        {
+          if (*it1 < *it2)
+          {
+            temp.push_back(*it1);
+            it1++;
+          }
+          else if (*it1 == *it2)
+          {
+            it1++; it2++;
+//            Hist.erase(Hist.begin() + *it1); //지우는데 들어가는 시간?
+            //지우고 나면 다른 인덱스 하나씩 당겨야한다!!
+          }
+          else
+          {
+            temp.push_back(*it2);
+            if (Hist[*it2].from == ID2)
+              Hist[*it2].from = ID1;
+            else Hist[*it2].to = ID1;
+            it2++;
+          }
+        }
+        if (it1 != id1_pos->second.log_idx.end() && it2 == id1_pos->second.log_idx.end())
+          for (;it1 != id1_pos->second.log_idx.end(); ++it1)
+            temp.push_back(*it1);
+        else if (it1 == id1_pos->second.log_idx.end() && it2 != id1_pos->second.log_idx.end())
+        {
+          for (;it2 != id2_pos->second.log_idx.end(); ++it2)
+          {
+            temp.push_back(*it2);
+            if (Hist[*it2].from == ID2)
+              Hist[*it2].from = ID1;
+            else Hist[*it2].to = ID1;
+          }
+        }
+
+        id1_pos->second.log_idx = temp;
+        T.erase(ID2);
+      }
+    }
   }
 
   return 0;
